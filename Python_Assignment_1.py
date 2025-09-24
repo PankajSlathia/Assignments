825bbbf0129dc5f3fa1cce4310e846393c4fdc50
PankajSingh <shamlislathia1039@gmail.com>
2025-09-24 13:24:02 +0530

    updated

--- Python_Assignment_1.py
+++ Python_Assignment_1.py
@@ -1,1 +1,26 @@
-# Python Assignment 1 Task 1: Perform Basic Mathematical Operations
+# Python Assignment 1
+
+
+# Task 1: Perform Basic Mathematical Operations
+
+
+num_1 = int(input("Enter the first number: "))
+num_2 = int(input("Enter the second number: "))
+
+addition = num_1 + num_2
+subtraction = num_1 - num_2
+multiplication = num_1 * num_2
+division = num_1 / num_2
+print("Addition: ", addition)
+print("Subtraction: ", subtraction)
+print("Multiplication: ", multiplication)
+print("Division: ", division)
+
+
+
+# Task 2: Create a Personalized Greeting
+
+name = input("Enter your fist name: ")
+last_name = input("Enter your last name: ")
+print("Hello, " + name + " " + last_name + "!" + "Welcome to the Python Program")
+print(f"Hello {name} {last_name}! Welcome to the Python Program")
